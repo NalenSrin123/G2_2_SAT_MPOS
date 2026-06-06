908a39c3c0da5ea95e93fa21bf9d8c4867d30672
hemsoksombrathna69 <hemsoksombrathna123@gmail.com>
2026-06-06 15:39:10 +0700

    fix some error

--- src/pages/dashboard/productsManagement/ProductManagement.vue
+++ src/pages/dashboard/productsManagement/ProductManagement.vue
@@ -35,7 +35,16 @@
       class="fixed inset-0 bg-black/50 flex items-center justify-center z-50"
     >
       <div class="bg-white rounded-xl p-6 w-full max-w-md">
-        <h2 class="text-xl font-bold mb-4">Add Product</h2>
+
+        <div class="flex justify-between mb-4">
+          <h2 class="text-xl font-bold">Add Product</h2>
+          <button
+            @click="showAddModal = false"
+            class="px-4 py-2 border rounded-lg bg-red-500 text-white hover:bg-red-600"
+          >
+            X
+          </button>
+        </div>
         <div class="space-y-3">
           <!-- IMAGE -->
           <label for="image">Image</label>
@@ -109,7 +118,15 @@
     >
       <div class="bg-white rounded-xl p-6 w-full max-w-md">
 
-        <h2 class="text-xl font-bold mb-4">Edit Product</h2>
+        <div class="flex justify-between mb-4">
+          <h2 class="text-xl font-bold">Edit Product</h2>
+          <button
+            @click="showEditModal = false"
+            class="px-4 py-2 border rounded-lg bg-red-500 text-white hover:bg-red-600"
+          >
+            X
+          </button>
+        </div>
 
         <div class="space-y-3">
 
@@ -169,7 +186,15 @@
     >
       <div class="bg-white p-6 rounded-lg w-96 shadow-lg">
 
-        <h2 class="text-lg font-bold text-red-600">Delete Product</h2>
+        <div class="flex justify-between mb-4">
+          <h2 class="text-lg font-bold text-red-600">Delete Product</h2>
+          <button
+            @click="closeModal"
+            class="px-4 py-2 border rounded-lg bg-red-500 text-white hover:bg-red-600"
+          >
+            X
+          </button>
+        </div>
 
         <p class="text-gray-600 mt-2">
           Are you sure you want to delete
