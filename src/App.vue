9f4c1f1e7de991c4192d63eda8fd264f2b1d1a9c
Yeng Srynich <yengsreynick@gmail.com>
2026-06-13 16:40:48 +0700

    design history page

--- src/App.vue
+++ src/App.vue
@@ -2,19 +2,23 @@
 import { computed } from 'vue'
 import { useRoute } from 'vue-router'
 import Sidebar from './components/Sidebar.vue'
+import HistoryPage from './components/HistoryPage.vue'
 
 const route = useRoute()
 const showSidebar = computed(() => route.meta.layout === 'dashboard')
 </script>
 
 <template>
-  <div v-if="showSidebar" class="flex min-h-screen bg-[#f4f6f9] text-slate-900">
+  <div>
+    <HistoryPage/>
+  </div>
+  <div v-if="false" class="flex min-h-screen bg-[#f4f6f9] text-slate-900">
     <Sidebar />
     <main class="min-w-0 flex-1 overflow-x-hidden">
       <RouterView />
     </main>
   </div>
 
-  <RouterView v-else />
+  <RouterView v-else-if="false" />
 </template>
 
